3d989102b698cb7a9671a7382235a174923c37a2
Joshua Williamson <jlwilliamson95@gmail.com>
2018-09-01 00:55:18 -0600

    Dev camp toop initial commit

---
+++ creating-a-ruby-gem/Williamson_devcamp_view_tool/lib/Williamson_devcamp_view_tool.rb
@@ -0,0 +1,5 @@
+require "Williamson_devcamp_view_tool/version"
+
+module WilliamsonDevcampViewTool
+  # Your code goes here...
+end
---
+++ creating-a-ruby-gem/Williamson_devcamp_view_tool/lib/Williamson_devcamp_view_tool/version.rb
@@ -0,0 +1,3 @@
+module WilliamsonDevcampViewTool
+  VERSION = "0.1.0"
+end
